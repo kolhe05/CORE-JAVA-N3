ef28a33afead61e70cf243de34044b20bddacb86
Student <Student@IOE-AIDS-184>
2023-02-01 13:57:17 +0530

    First Commit

---
+++ dayone/src/org/tnsif/intro/SimpleJavaProgramDemo.java
@@ -0,0 +1,11 @@
+package org.tnsif.intro;
+
+public class SimpleJavaProgramDemo {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to Java Fullstack C2CT Program");
+		// TODO Auto-generated method stub
+
+	}
+
+}
