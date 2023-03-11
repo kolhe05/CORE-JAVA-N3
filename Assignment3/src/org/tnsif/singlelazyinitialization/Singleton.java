9130617ec77202f7885fcfa46adaa925c19ea929
Gauri Kolhe <124130010+kolhe05@users.noreply.github.com>
2023-03-11 11:14:58 +0530

    first commit

---
+++ Assignment3/src/org/tnsif/singlelazyinitialization/Singleton.java
@@ -0,0 +1,29 @@
+package org.tnsif.singlelazyinitialization;
+
+public class Singleton {
+	private static Singleton INSTANCE = null;
+	 
+	/*
+	 * private constructor so that
+	 * preventing instance creation from other class
+	 */
+	private Singleton() {
+ 
+	}
+ 
+	/**
+	 * This method has global access to return
+	 * Single instance within JVM
+	 * @return Singleton Instance
+	 */
+	public static Singleton getInstance() {
+		//If Instance is null then only instantiate
+		if (INSTANCE == null) {
+			INSTANCE = new Singleton();
+		}
+		return INSTANCE;
+ 
+	}
+}
+
+
